70348301bd3556ed72b87dcbe25aab1fa74866c6
Pai314 <168731575+neipia271828@users.noreply.github.com>
2025-04-11 09:00:18 +0900

    update

--- NN/NN.py
+++ NN/NN.py
@@ -25,6 +25,7 @@ def test(func, gate_name):
         for j in range(2):
             print(f"({i},{j}):{func.NN(i,j)}")
 
+"""
 #テスト実行
 test(AND,"AND")
 test(OR,"OR")
@@ -42,3 +43,14 @@ XOR_3 = NeuralNetwork(-1,2,1)
 target_func = [XOR_1,XOR_2,XOR_3]
 for i in target_func:
     test(i,"")
+
+"""
+
+import random
+
+for i in range(10000):
+    func = NeuralNetwork(random.uniform(-10,10),random.uniform(-10,10),random.uniform(-10,10))
+    if func.NN(1,0) == 1 and func.NN(0,1) == 1 and func.NN(1,1) != 1 and func.NN(0,0) != 1:
+        print("hit")
+        print(f"XORis({func.w1},{func.w2},{func.theta})")
+        break
